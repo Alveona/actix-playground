9c72be5f9d20e9ca6162dcf0889d945eca2ca204
Alveona <pomavau@yandex.ru>
2020-02-15 02:05:49 +0300

    Examples with deserialization and query params

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 #[macro_use] extern crate log;
+#[allow(dead_code)]
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
-use serde::{Deserialize, Serialize};
+use serde::{ser, de, Deserialize, Deserializer, Serialize};
 mod constants;
+use qstring::QString;
+use std::fmt::{self, Display};
 
 #[derive(Serialize, Deserialize)]
 struct Response {
     message: String,
 }
 
-async fn index() -> impl Responder {
+#[derive(Deserialize)]
+struct QueryStruct{
+    #[serde(default, deserialize_with = "deserialize_some")]
+    text: Option<String>,
+    #[serde(default, deserialize_with = "deserialize_some")]
+    id: Option<u64>
+}
+
+fn deserialize_some<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+    where T: Deserialize<'de>,
+          D: Deserializer<'de>
+{
+    Deserialize::deserialize(deserializer).map(Some)
+}
+
+async fn index(web::Query(query): web::Query<QueryStruct>) -> impl Responder {
+    if let Some(_text) = query.text {
+        println!("{:?}", _text);
+    }
+    
     HttpResponse::Ok().json(Response{
         message: "Hello, Actix!".to_string()
     })
@@ -24,16 +46,14 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
 
-
-    let connection_string = format!("{}:{}", constants::HOST, constants::PORT);
+    let connection_string = format!("{}:{}", constants::HOST, constants::PORT);    
+    
     println!("Running web server on {}", connection_string);
-
-
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
+            .route("/hw", web::get().to(index2))
     })
     .bind(connection_string)?
     .run()
